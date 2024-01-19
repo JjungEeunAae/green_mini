6078c4e20cd901c338f23bd2c626e96d4b40f5f7
EunAe <wjddmsdo77@gmail.com>
2024-01-19 18:07:39 +0900

    login 진행중

--- greenProject/src/db/user/User.java
+++ greenProject/src/db/user/User.java
@@ -1,8 +1,13 @@
 package db.user;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import db.DBConnection;
 
 public class User {
 	private Connection conn;	// data base connection
@@ -21,7 +26,44 @@ public class User {
 	
 	
 	
+	// 로그인 ( 은애 )
+	public String login(String id, String pw) {
+		try {
+			conn = DriverManager.getConnection(DBConnection.JDBC_URL, DBConnection.USERNAME, DBConnection.PASSWORD);
+			sql = "SELECT * FROM test WHERE id = ?";
+			PreparedStatement pre = conn.prepareStatement(sql);
+			// 파라미터 설정
+			pre.setString(1, id);
+            
+            // 쿼리 실행
+            try (ResultSet resultSet = pre.executeQuery()) {
+            	if (resultSet.next()) {
+                    // 사용자가 존재하는 경우 비밀번호 확인
+                    String storedPassword = resultSet.getString("pw");
+                    if (pw.equals(storedPassword)) {
+                        // 비밀번호가 일치하는 경우 로그인 성공
+                        return "SUCCESS";
+                    } else {
+                        // 비밀번호가 일치하지 않는 경우
+                        return "WRONG_PASSWORD";
+                    }
+                } else {
+                    // 사용자가 존재하지 않는 경우
+                    return "USER_NOT_FOUND";
+                }
+            }
+            
+		} catch (SQLException e) {
+			e.printStackTrace();
+		};
+		
+		return "ERROR";
+	};
 	
+	
+	
+	
+	// 회원탈퇴 ( 미정 )
 	
 	
 	
