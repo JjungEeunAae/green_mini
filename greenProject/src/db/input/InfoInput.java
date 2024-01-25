01bd3796f314ca1fe55428c7ea58fd72fc1c5d42
JjungEeunAae <wjddmsdo77@naver.com>
2024-01-25 23:44:45 +0900

    main: 메뉴 클래스 생성, 로그인 성공 시 아이디가 아닌 이름으로 환영해줌, 유저에게 입력받는 부분도 따로 클래스로 뺌

--- greenProject/src/db/input/InfoInput.java
+++ greenProject/src/db/input/InfoInput.java
@@ -2,32 +2,74 @@ package db.input;
 
 import java.util.Scanner;
 
+import db.user.User;
 import db.user.UserVO;
+import style.Font;
 
 public class InfoInput {
-	private Scanner sc = new Scanner(System.in);
-
+	Scanner sc = new Scanner(System.in);
 	// 로그인 시 입력받을 값
 	public UserVO loginInput() {
-		System.out.println("아이디를 입력해주세요.");
+		User user = new User();
+		
+		System.out.println(">> 아 이 디 입력 <<");
 		String id = sc.next();
-		System.out.println("비밀번호를 입력해주세요.");
+		System.out.println(">> 비밀번호 입력 <<");
 		String pw = sc.next();
 		
-		return new UserVO(id, pw);
+		return user.userInfo(id, pw);
 	}
 	
 	// 회원가입 시 입력받을 값
 	public UserVO signUpInput() {
 		System.out.println("회원가입 진행");
-		System.out.println("아이디 입력>");
-		String sign_id = sc.next();
-		System.out.println("비번 입력>");
-		String sign_pw = sc.next();
-		System.out.println("이름 입력>");
-		String sign_name = sc.next();
+		System.out.println(">> 아이디 입력 <<");
+		String sign_id = sc.nextLine();
+		System.out.println(">> 비밀번호 입력 <<");
+		String sign_pw = sc.nextLine();
+		System.out.println(">> 이름 입력 <<");
+		String sign_name = sc.nextLine();
 		
 		return new UserVO(sign_id, sign_pw, sign_name);
+	}
+	
+	// 회원탈퇴 시 유저의 비번을 입력받음
+	public String pwCheck() {
+		System.out.println("비밀번호를 입력해주세요.");
+		String pwCheck = sc.next();
+		return pwCheck;
+	}
+	
+	// 회원탈퇴 시 랜덤숫자를 받아 정상 탈퇴를 진행함
+	// 랜덤 1차 입력
+	public boolean randomIntCheck() {
+		boolean result = false;
+		int random = (int) (Math.random() * 555) + 1;
+		
+		System.out.println("탈퇴 진행을 위해 번호를 입력해주세요.");
+		System.out.println("입력하기 >> [  " + random + "  ]");
+		int randomNumberCheck = sc.nextInt();
+		
+		if (randomNumberCheck != random) {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	// 랜덤 숫자 2차 입력
+	public boolean randomIntDoubleCheck() {
+		boolean result = false;
+		int randomDouble = (int) (Math.random() * 555) + 1;
+		System.out.println(Font.BACKGROUND_BLACK + Font.FONT_RED + "틀렸습니다! 번호를 재입력해주세요."
+				+ Font.RESET);
+		System.out.println("입력하기 >> [  " + randomDouble + "  ]");
+		int randomNumberDoubleCheck = sc.nextInt();
+		
+		if(randomNumberDoubleCheck != randomDouble) {
+			result = true;
+		}
+		return result;
 	}
 	
 }
