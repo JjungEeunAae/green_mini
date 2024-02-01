bb62363f731c44a285373948167be472c3457804
sdqwe12 <dudghcjswo3@gmail.com>
2024-02-01 16:48:09 +0900

    test

---
+++ greenProject/src/db/user/MakeId.java
@@ -0,0 +1,12 @@
+package db.user;
+
+import java.util.Scanner;
+
+public class MakeId {
+	Scanner scan = new Scanner(System.in);
+	User      user  = new User();
+	
+	public void MakeId() {
+		
+	}
+}
