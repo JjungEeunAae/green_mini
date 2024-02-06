456529216e3047562eb021115d3c48d341eb01f8
hee <qpqp4250@naver.com>
2024-02-06 12:14:14 +0900

    수정

--- greenProject/src/db/guestBook/GuestBook.java
+++ greenProject/src/db/guestBook/GuestBook.java
@@ -17,8 +17,11 @@ public class GuestBook {
 	public void listView() {
 		try {
 			// 1. db와 연결한다.			       순서대로 url, userid, userpw
+			
 			conn = DriverManager.getConnection(DBConnection.JDBC_URL, DBConnection.USERNAME, DBConnection.PASSWORD);
+			
 			// 2. 쿼리를 짜서 java를 통해 sql 쿼리를 실행한다.
+			
 			pr = conn.prepareStatement("SELECT id"
 										  + ", score"
 										  + ", DATE_FORMAT(in_date, '%Y-%m-%d') AS \"in_date\" "
@@ -28,10 +31,15 @@ public class GuestBook {
 			
 			// 3. 나는 어떠한 모습으로 유저에게 내용을 보여줄 것인가?
 			// rs가 가지고 있는 결과값이 있을 땐 true, 없을 땐 false
+			
 			int num = 1;
+			
 			System.out.println("순위\t아이디\t점수\t등록일");
+			
 			while (rs.next()) {
-				System.out.println((num++) + "\t" + rs.getString("id") + "\t"  + rs.getString("score") + "\t" + rs.getString("in_date"));
+				System.out.println((num++) + "\t" + rs.getString("id") + "\t"  
+			                                      + rs.getString("score") 
+		                                          + "\t" + rs.getString("in_date"));
 			}
 			System.out.println();
 		} catch (SQLException e) {
